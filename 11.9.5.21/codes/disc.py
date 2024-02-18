26d732535c7d6c58de660b04eac6065cd65d97c7
Srija04092006 <155562681+Srija04092006@users.noreply.github.com>
2024-02-18 12:21:22 +0530

    Add files via upload

---
+++ 11.9.5.21/codes/disc.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read data from the text file
+with open("dat.txt", "r") as file:
+    data = [line.strip().split() for line in file]
+
+# Separate values of n and x(n)
+n_values, x_values = zip(*[(int(line[0]), float(line[1])) for line in data])
+
+# Plot the stem graph without grid
+plt.stem(n_values, x_values, basefmt='b-', linefmt='r-', markerfmt='ro')
+plt.title('')
+plt.xlabel('n')
+plt.ylabel('x_2(n)')
+plt.grid(False)  # Remove the grid
+plt.show()
+
